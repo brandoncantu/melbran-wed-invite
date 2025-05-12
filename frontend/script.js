b86823e5ea64ef73886fe120e691a9bef7db28ad
Brandon Cantu Lozano <74885848+brandoncantu@users.noreply.github.com>
2025-05-12 12:55:22 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
     slides[0].classList.add('active'); // Make the first slide active initially
     
     function showSlides(control) {
-        let prevIndex = slideIndex;
+        if(slideIndex==0){
+            let prevIndex = slides.length;
+        }else{
+            let prevIndex = slideIndex;
+        }
         if(control==0){
             slideIndex--;
         }else{
