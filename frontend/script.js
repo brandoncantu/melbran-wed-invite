9f344d9b852abd0587ea223cd831189e0ac3a6a3
Brandon Cantu Lozano <74885848+brandoncantu@users.noreply.github.com>
2025-05-14 12:45:40 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -142,6 +142,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     sheetLineInput.value = data.sheetLine;
                     familyNameInput.disabled = true;
                     familyNameInput.style.border = 'none';
+		    familyInput.style.maxWidth = 'fit-content';
                     if (data.accept == "accept" || data.accept == "decline") {
                         attendanceSelect.style.display = 'none';
                         submitButton.disabled = true;
